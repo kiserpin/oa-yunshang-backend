e2ad34446a1545b349001ee4fdd7b57e5d1e4a6e
Administrator <345869698@qq.com>
2024-03-23 09:55:49 +0800

    no just test

--- service-oa/src/test/java/com/kmun/auth/TestMpDemo1.java
+++ service-oa/src/test/java/com/kmun/auth/TestMpDemo1.java
@@ -53,7 +53,7 @@ public class TestMpDemo1 {
 
     @Test
     public void testSelect1() {
-        //测试
+        //测试1
         QueryWrapper<SysRole> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("role_code", "role");
         List<SysRole> users = sysRoleMapper.selectList(queryWrapper);
