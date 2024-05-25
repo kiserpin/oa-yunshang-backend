ed3e56efcf73270531ecb5ac172b280e8554c939
gtf <12345678@qq.com>
2024-05-25 16:42:57 +0800

    完善角色管理

--- service-oa/src/main/java/com/kmun/auth/service/SysRoleService.java
+++ service-oa/src/main/java/com/kmun/auth/service/SysRoleService.java
@@ -2,9 +2,22 @@ package com.kmun.auth.service;
 
 import com.kmun.model.system.SysRole;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.kmun.vo.AssginRoleVo;
 
 import java.util.Map;
 
 public interface SysRoleService extends IService<SysRole> {
+    /**
+     * 根据用户获取角色数据
+     * @param userId
+     * @return
+     */
+    Map<String, Object> findRoleByAdminId(Long userId);
+
+    /**
+     * 分配角色
+     * @param assginRoleVo
+     */
+    void doAssign(AssginRoleVo assginRoleVo);
 
 }
